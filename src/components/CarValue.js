952502391825f619cef550ac6e59541bceabb4ea
NitinNitinRaj <bnitinraj@gmail.com>
2023-08-25 19:02:41 +0530

    modified:   src/components/CarValue.js

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+
 export default function CarValue() {
-  return (
-    <div>
-      <h1>CarValue</h1>
-    </div>
-  );
+  const { data, searchTerm } = useSelector((state) => state.cars);
+
+  const totalValue = data.reduce((sum, car) => {
+    if (car.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+      return sum + car.cost;
+    }
+    return sum;
+  }, 0);
+
+  return <p className="car-value">{`Total Value: $${totalValue}`}</p>;
 }
