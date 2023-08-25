ac853be99fbd9e9fdf307413571b0ce71517e0e3
NitinNitinRaj <bnitinraj@gmail.com>
2023-08-25 19:43:57 +0530

    Changes to be committed: 	modified:   src/components/CarList.js

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,31 +3,27 @@ import { removeCar } from "../store";
 
 export default function CarList() {
   const dispatch = useDispatch();
-  const data = useSelector(({ cars: { data, searchTerm } }) => {
-    return data.filter((car) =>
-      car.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  });
-
-  const { name } = useSelector((state) => state.carForm);
+  const { data, name } = useSelector(
+    ({ carForm, cars: { data, searchTerm } }) => {
+      const filterData = data.filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      return { data: filterData, name: carForm.name };
+    }
+  );
 
   const handleCarDelete = (car) => {
     dispatch(removeCar(car));
   };
 
   const renderedCarsList = data.map((car) => {
+    const bold = car.name.toLowerCase().includes(name.toLowerCase()) && name;
     return (
       <div key={car.id} className="panel">
-        {car.name.toLowerCase().startsWith(name.toLowerCase()) &&
-        name !== "" ? (
-          <p className="bold">
-            {car.name} - ${car.cost}
-          </p>
-        ) : (
-          <p>
-            {car.name} - ${car.cost}
-          </p>
-        )}
+        <p className={`${bold && "bold"}`}>
+          {car.name} - ${car.cost}
+        </p>
+
         <button
           onClick={() => handleCarDelete(car)}
           className="button is-danger"
